c6c3e3c5b2c796b6718e4839fddf091687378c38
liubing <lbingg@hotmail.com>
2018-05-17 18:01:18 +0800

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,25 +1,19 @@
 # -*- coding: utf-8 -*-
 
-# 主要是需要moviepy这个库
 from moviepy.editor import *
 import os
 
-# 定义一个数组
 L = []
 
-# 访问 video 文件夹 (假设视频都放在这里面)
+# 遍历 video 文件夹
 for root, dirs, files in os.walk("./video"):
     # 按文件名排序
     files.sort()
-    # 遍历所有文件
     for file in files:
         # 如果后缀名为 .mp4
         if os.path.splitext(file)[1] == '.mp4':
-            # 拼接成完整路径
             filePath = os.path.join(root, file)
-            # 载入视频
             video = VideoFileClip(filePath)
-            # 添加到数组
             L.append(video)
 
 # 拼接视频
